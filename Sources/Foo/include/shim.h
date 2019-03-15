22cb9893b57525bc6a51e0ed8e2500126feca56a
araki <araki@qoncept.co.jp>
2019-03-15 15:11:20 +0900

    Put dummy.c, module.modulemap

--- Sources/Foo/include/shim.h
+++ Sources/Foo/include/shim.h
@@ -5,7 +5,7 @@
 #ifndef shim_h
 #define shim_h
 
-#include <Foo.h>
-#include <Bar.h>
+#include <FooBar/Foo.h>
+#include <FooBar/Bar.h>
 
 #endif /* shim_h */
--- Tests/FooTests/FooTests.swift
+++ Tests/FooTests/FooTests.swift
@@ -6,7 +6,7 @@ final class FooTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Foo().text, "Hello, World!")
+        foo(0)
     }
 
     static var allTests = [
